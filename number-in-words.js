f0c120b54f431dee63a12c80e8107930bd4b5c6c
memorvin <alvina.tjia@yahoo.com>
2019-10-28 18:44:48 +0700

    update number-in-words

--- number-in-words.js
+++ number-in-words.js
@@ -14,19 +14,19 @@ function inWords(num) {
   } else if (num < 1000) {
     return lib[Math.floor(num / 100)] + ' ratus ' + inWords(num % 100);
   } else if (num < 2000) {
-    return 'seribu ' + inWords(Math.floor(num - 1000));
+    return 'seribu ' + inWords(num - 1000);
   } else if (num < 1000000) {
     return inWords(Math.floor(num / 1000)) + ' ribu ' + inWords(num % 1000);  
   } else if (num < 2000000) {
-    return 'sejuta ' + inWords(Math.floor(num - 1000000));
+    return 'sejuta ' + inWords(num - 1000000);
   } else if (num < 1000000000) {
     return inWords(Math.floor(num / 1000000)) + ' juta ' + inWords(num % 1000000);  
   } else if (num < 2000000000) {
-    return 'semilyar ' + inWords(Math.floor(num - 1000000000));
+    return 'semilyar ' + inWords(num - 1000000000);
   } else if (num < 1000000000000) {
     return inWords(Math.floor(num / 1000000000)) + ' milyar ' + inWords(num % 1000000000);  
   } else if (num < 2000000000000) {
-    return 'setriliun ' + inWords(Math.floor(num - 1000000000000));
+    return 'setriliun ' + inWords(num - 1000000000000);
   } else if (num < 1000000000000000) {
     return inWords(Math.floor(num / 1000000000000)) + ' triliun ' + inWords(num % 1000000000000);  
   }
